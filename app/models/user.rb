0a652090d6aa72c0992bc206bfeace86bc1fdb5b
sammyValsangkar <sameer@cart91.com>
2017-03-03 17:01:24 +0530

    assignment finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -104,7 +104,8 @@ class User < ApplicationRecord
 
   # See "Following users" for the full implementation.
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
+                    following_ids: following_ids, user_id: id)
   end
 
   # Follows a user.
