0e0a062b2b11258017666f4d34f89f234dcbf539
sammyValsangkar <sameer@cart91.com>
2017-03-03 10:50:53 +0530

    activating user finished

---
+++ config/initializers/smtp_settings.rb
@@ -0,0 +1,9 @@
+Rails.application.config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'sameer@cart91.com',
+  password:             'yahovanissi',
+  authentication:       :plain,
+  enable_starttls_auto: true
+}
