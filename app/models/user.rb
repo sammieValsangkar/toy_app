a9128923c0f125d2695d6314de823c04a6e7da78
sammyValsangkar <sameer@cart91.com>
2017-03-03 11:28:41 +0530

    password reset complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -88,4 +88,9 @@ class User < ApplicationRecord
     update_attribute(:reset_sent_at, Time.zone.now)
   end
 
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
 end
